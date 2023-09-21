b9ffcbe4a4e5f0103d89710edb271a7f21de4b7c
Marco Zellini <zellinimarco@gmail.com>
2023-09-21 15:14:33 +0200

    Bonus 1 - Add First Component Test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <script>
 
+// import HelloWorld from './components/HelloWorld.vue'
+import AppTitle from './components/AppTitle.vue'
+
 export default {
   data() {
     return {
-      text: "Hello Vite"
+      text: "Hello Vite",
+      count: 0
     }
-  }
+  },
+  components: {
+    AppTitle
+  },
+
 }
 
 </script>
 
 <template>
   <h1>{{ text }}</h1>
+  <!--   <HelloWorld msg="MessaggioTest" /> -->
+  <AppTitle title="La mia prima app con Vite!" />
 </template>
 
 <style></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,12 @@
+<script>
+
+export default {
+    props: ['title']
+}
+
+</script>
+
+
+<template>
+    <h2>{{ title }}</h2>
+</template>
